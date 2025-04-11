2222237afa2c3b2e089a6ffdbde1f9076afded4a
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-11 09:57:51 +0200

    refactor: Remove TransactionService provider from TransactionTimelineComponent

--- apps/transactions-app/src/app/transaction-timeline/transaction-timeline.component.ts
+++ apps/transactions-app/src/app/transaction-timeline/transaction-timeline.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
 import type { GroupedTransactions } from './transaction-timeline.model';
-import { TransactionService } from './transaction.service';
 import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-timeline',
   templateUrl: './transaction-timeline.component.html',
   styleUrls: ['./transaction-timeline.component.scss'],
-  providers: [TransactionService],
   imports: [NgFor, CommonModule],
 })
 export class TransactionTimelineComponent {
