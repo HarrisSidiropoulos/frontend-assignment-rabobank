8ca3d5a51dde99ae3c2f777b7887f4fcdf67951b
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-11 16:04:42 +0200

    fix: Correct welcome message in AppComponent test to reflect accurate app title

--- apps/transactions-app/src/app/app.component.spec.ts
+++ apps/transactions-app/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome transactions-app'
+      'Welcome to transaction timeline app'
     );
   });
 
