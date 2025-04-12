364d4908d994288b74ec6e591efd679e4be21bba
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-12 11:21:15 +0200

    refactor: Introduce reusable card component and update transaction views to use it

--- apps/transactions-app/src/app/transaction-timeline/transaction-timeline.component.ts
+++ apps/transactions-app/src/app/transaction-timeline/transaction-timeline.component.ts
@@ -3,12 +3,13 @@ import type { GroupedTransactions } from '../shared/transaction.model';
 import { CommonModule, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CovertInEurPipe } from '../shared/covert-in-eur.pipe';
+import { CardComponent } from '../shared/ui/card/card.component';
 
 @Component({
   selector: 'app-transaction-timeline',
   templateUrl: './transaction-timeline.component.html',
   styleUrls: ['./transaction-timeline.component.scss'],
-  imports: [NgFor, CommonModule, RouterLink, CovertInEurPipe],
+  imports: [NgFor, CommonModule, RouterLink, CovertInEurPipe, CardComponent],
 })
 export class TransactionTimelineComponent {
   @Input() groupedTransactions: GroupedTransactions[] = [];
--- apps/transactions-app/src/app/transaction/transaction.component.ts
+++ apps/transactions-app/src/app/transaction/transaction.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 import { Transaction } from '../shared/transaction.model';
 import { CovertInEurPipe } from '../shared/covert-in-eur.pipe';
 import { RouterLink } from '@angular/router';
+import { CardComponent } from '../shared/ui/card/card.component';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss'],
-  imports: [CommonModule, CovertInEurPipe, RouterLink],
+  imports: [CommonModule, CovertInEurPipe, RouterLink, CardComponent],
 })
 export class TransactionComponent {
   @Input({ required: true }) transaction!: Transaction;
