f0f5c187903d25905a62e98e2ad11522dc465b48
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-12 08:09:23 +0200

    refactor: Update transaction resolver to return a single transaction or undefined

--- apps/transactions-app/src/app/transaction/transaction.resolver.ts
+++ apps/transactions-app/src/app/transaction/transaction.resolver.ts
@@ -4,18 +4,18 @@ import { map } from 'rxjs/operators';
 import type { Transaction } from '../common/transaction.model';
 import { TransactionService } from '../common/transaction.service';
 
-export const resolveTransaction: ResolveFn<Transaction> = (route) => {
+export const resolveTransaction: ResolveFn<Transaction | undefined> = (
+  route
+) => {
   const transactionService = inject(TransactionService);
   const { date, id } = route.params;
 
   return transactionService.getAllTransactions().pipe(
     map((transactions) => {
-      return transactions.filter((tx) => {
-        const transactionDate = new Date(tx.timestamp)
-          .toISOString()
-          .slice(0, 10);
-        return transactionDate === date && tx.id.toString() === id;
-      })?.[0];
+      return transactions.find((tx) => {
+        const txDate = new Date(tx.timestamp).toISOString().slice(0, 10);
+        return txDate === date && tx.id.toString() === id;
+      });
     })
   );
 };
