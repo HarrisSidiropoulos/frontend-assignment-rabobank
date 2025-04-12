bf3cadcda8731595ee4dc7c05e4d72363ed6be9a
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-12 07:59:52 +0200

    refactor: Add back link to transaction detail component and style it for better navigation

--- apps/transactions-app/src/app/transaction/transaction.component.ts
+++ apps/transactions-app/src/app/transaction/transaction.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Transaction } from '../common/transaction.model';
 import { CovertInEurPipe } from '../common/covert-in-eur.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss'],
-  imports: [CommonModule, CovertInEurPipe],
+  imports: [CommonModule, CovertInEurPipe, RouterLink],
 })
 export class TransactionComponent {
   @Input({ required: true }) transaction!: Transaction;
