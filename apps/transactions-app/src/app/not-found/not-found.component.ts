c411934ca5ad96105c1eb7939708811f51c2ba59
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-04-12 11:24:30 +0200

    refactor: Replace section with card component in NotFound view and remove unused styles

--- apps/transactions-app/src/app/not-found/not-found.component.ts
+++ apps/transactions-app/src/app/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../shared/ui/card/card.component';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
+  imports: [CardComponent],
 })
 export class NotFoundComponent {}
